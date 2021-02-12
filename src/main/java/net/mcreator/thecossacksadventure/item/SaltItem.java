0faf46d52f53640b65a052c2b6ee3dc1970ea3cf
Danyaruko <danilpatana1905@gmail.com>
2021-02-12 20:12:19 +0200

    Disabled attack for Salt

--- src/main/java/net/mcreator/thecossacksadventure/item/SaltItem.java
+++ src/main/java/net/mcreator/thecossacksadventure/item/SaltItem.java
@@ -1,24 +1,29 @@
 
 package net.mcreator.thecossacksadventure.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.thecossacksadventure.TheCossacksAdventureModElements;
+
 @TheCossacksAdventureModElements.ModElement.Tag
 public class SaltItem extends TheCossacksAdventureModElements.ModElement {
-
 	@ObjectHolder("the_cossacks_adventure:salt")
 	public static final Item block = null;
-
 	public SaltItem(TheCossacksAdventureModElements instance) {
 		super(instance, 5);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("salt");
@@ -38,19 +43,5 @@ public class SaltItem extends TheCossacksAdventureModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
-		@Override
-		public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot) {
-			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot);
-			if (slot == EquipmentSlotType.MAINHAND) {
-				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "item_damage", (double) -1.5, AttributeModifier.Operation.ADDITION));
-				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
-						new AttributeModifier(ATTACK_SPEED_MODIFIER, "item_attack_speed", -2.4, AttributeModifier.Operation.ADDITION));
-			}
-			return multimap;
-		}
-
 	}
-
 }
