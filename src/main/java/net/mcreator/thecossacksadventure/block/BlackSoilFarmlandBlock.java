363c333b28ae0d3428808de45ad87b2d869f7910
work <work@DESKTOP>
2021-02-12 20:49:42 +0200

    MCreator Merge commit '996e686bc28efa6d081d87053a0faccc8917c598' into bc16b9871e255219cea715450d8ee7a9e91e1ca0

--- src/main/java/net/mcreator/thecossacksadventure/block/BlackSoilFarmlandBlock.java
+++ src/main/java/net/mcreator/thecossacksadventure/block/BlackSoilFarmlandBlock.java
@@ -1,41 +1,17 @@
 
 package net.mcreator.thecossacksadventure.block;
 
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.common.ToolType;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
-
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.util.math.shapes.VoxelShapes;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Item;
-import net.minecraft.item.BlockItem;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Block;
-
-import net.mcreator.thecossacksadventure.TheCossacksAdventureModElements;
-
-import java.util.List;
-import java.util.Collections;
 
 @TheCossacksAdventureModElements.ModElement.Tag
 public class BlackSoilFarmlandBlock extends TheCossacksAdventureModElements.ModElement {
+
 	@ObjectHolder("the_cossacks_adventure:black_soil_farmland")
 	public static final Block block = null;
+
 	public BlackSoilFarmlandBlock(TheCossacksAdventureModElements instance) {
 		super(instance, 3);
+
 	}
 
 	@Override
@@ -50,10 +26,15 @@ public class BlackSoilFarmlandBlock extends TheCossacksAdventureModElements.ModE
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
+
 	public static class CustomBlock extends Block {
+
 		public CustomBlock() {
-			super(Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
-					.harvestTool(ToolType.SHOVEL).notSolid());
+			super(
+
+					Block.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
+							.harvestTool(ToolType.SHOVEL).notSolid());
+
 			setRegistryName("black_soil_farmland");
 		}
 
@@ -75,10 +56,13 @@ public class BlackSoilFarmlandBlock extends TheCossacksAdventureModElements.ModE
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(BlackSoilNoGrassBlock.block, (int) (2)));
 		}
+
 	}
+
 }
