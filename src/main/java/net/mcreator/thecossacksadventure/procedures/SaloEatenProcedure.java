422d305176df62fb8b263324a0d3cf8a3f555a16
Danyaruko <danilpatana1905@gmail.com>
2021-02-12 19:38:17 +0200

    MCreator Merge commit 'cce5e814a55a7f2548fac29dd448096cd134bd50' into 21e2339adacf446db6920de2efe8bbf25998183e

--- src/main/java/net/mcreator/thecossacksadventure/procedures/SaloEatenProcedure.java
+++ src/main/java/net/mcreator/thecossacksadventure/procedures/SaloEatenProcedure.java
@@ -1,17 +1,11 @@
 package net.mcreator.thecossacksadventure.procedures;
 
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.Entity;
-
-import net.mcreator.thecossacksadventure.TheCossacksAdventureModElements;
-
-import java.util.Map;
-
 @TheCossacksAdventureModElements.ModElement.Tag
 public class SaloEatenProcedure extends TheCossacksAdventureModElements.ModElement {
+
 	public SaloEatenProcedure(TheCossacksAdventureModElements instance) {
 		super(instance, 6);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -20,12 +14,16 @@ public class SaloEatenProcedure extends TheCossacksAdventureModElements.ModEleme
 				System.err.println("Failed to load dependency entity for procedure SaloEaten!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
+
 		if (entity instanceof PlayerEntity)
 			((PlayerEntity) entity).addExperienceLevel((int) 5);
 		if (entity instanceof PlayerEntity && !entity.world.isRemote) {
 			((PlayerEntity) entity).sendStatusMessage(
 					new StringTextComponent("\u041C\u043C\u043C\u043C\u043C, \u0441\u0430\u043B\u044C\u0446\u0435\u0435\u0435\u0435..."), (true));
 		}
+
 	}
+
 }
