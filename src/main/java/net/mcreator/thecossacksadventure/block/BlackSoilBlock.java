8cd25e8d5ed103bab1633471012c2bd8e2d98f98
work <work@DESKTOP>
2021-02-12 18:47:08 +0200

    MCreator Merge commit 'e2e43002fd27d5b653d6b7342cf3a80b3dcff519' into af92e7aee96d7f02edf878586ac0be36f9d15668

--- src/main/java/net/mcreator/thecossacksadventure/block/BlackSoilBlock.java
+++ src/main/java/net/mcreator/thecossacksadventure/block/BlackSoilBlock.java
@@ -1,47 +1,17 @@
 
 package net.mcreator.thecossacksadventure.block;
 
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.common.IPlantable;
-
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.gen.placement.Placement;
-import net.minecraft.world.gen.placement.CountRangeConfig;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.OreFeature;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.Direction;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Item;
-import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Block;
-
-import net.mcreator.thecossacksadventure.TheCossacksAdventureModElements;
-
-import java.util.Random;
-import java.util.List;
-import java.util.Collections;
 
 @TheCossacksAdventureModElements.ModElement.Tag
 public class BlackSoilBlock extends TheCossacksAdventureModElements.ModElement {
+
 	@ObjectHolder("the_cossacks_adventure:black_soil")
 	public static final Block block = null;
+
 	public BlackSoilBlock(TheCossacksAdventureModElements instance) {
 		super(instance, 1);
+
 	}
 
 	@Override
@@ -50,9 +20,14 @@ public class BlackSoilBlock extends TheCossacksAdventureModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
+
 	public static class CustomBlock extends Block {
+
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			super(
+
+					Block.Properties.create(Material.ORGANIC).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+
 			setRegistryName("black_soil");
 		}
 
@@ -63,12 +38,15 @@ public class BlackSoilBlock extends TheCossacksAdventureModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
+
 	}
+
 	@Override
 	public void init(FMLCommonSetupEvent event) {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
@@ -83,15 +61,19 @@ public class BlackSoilBlock extends TheCossacksAdventureModElements.ModElement {
 				biomeCriteria = true;
 			if (!biomeCriteria)
 				continue;
+
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, new OreFeature(OreFeatureConfig::deserialize) {
 				@Override
 				public boolean place(IWorld world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
 					DimensionType dimensionType = world.getDimension().getType();
 					boolean dimensionCriteria = false;
+
 					if (dimensionType == DimensionType.OVERWORLD)
 						dimensionCriteria = true;
+
 					if (!dimensionCriteria)
 						return false;
+
 					return super.place(world, generator, rand, pos, config);
 				}
 			}.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.create("black_soil", "black_soil", blockAt -> {
@@ -104,4 +86,5 @@ public class BlackSoilBlock extends TheCossacksAdventureModElements.ModElement {
 			}), block.getDefaultState(), 16)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(20, 0, 0, 64))));
 		}
 	}
+
 }
